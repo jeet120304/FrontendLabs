40d0afc07033369a3fab2f7043a629967c3ebf6a
jeet120304 <goudaabhijeet1@gmail.com>
2025-02-20 21:13:58 +0530

    footertoFoot

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import "./footer.css"
+import "./Footer.css"
 import { FaInstagram, FaLinkedin, FaTwitter } from 'react-icons/fa'
 import { NavLink } from 'react-router-dom'
 import LabsLogo from "../../assets/images/blackbglogo1.png"
